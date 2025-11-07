03e3d7ce59c3b1cb1aabf5adfe636f9684bac4d0
MLuana725 <marialuana.sl962@gmail.com>
2025-11-07 16:23:24 -0300

    Criação do Model de Usuário

--- Apoia+/usuario/models.py
+++ Apoia+/usuario/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Usuario(models.Model):
+    TIPOS_USUARIO = [
+        ('ong', 'ONG'),
+        ('voluntario','Voluntário'),
+        ('admin','Administrador'),
+    ]
+
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    senha = models.CharField(max_length=128)
+    tipo_usuario = models.CharField(
+        max_length=20,
+        choices=TIPOS_USUARIO,
+        default='voluntario'
+    )
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"{self.nome} ({self.get_tipo_usuario_display()})"
