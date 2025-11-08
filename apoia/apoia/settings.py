af917e628437ddceb2261e44f4c4ac3f59bbcc23
Guilherme <gui16carvalho@gmail.com>
2025-11-08 18:39:16 -0300

    Conectar o django ao banco

--- apoia/apoia/settings.py
+++ apoia/apoia/settings.py
@@ -74,10 +74,15 @@ WSGI_APPLICATION = 'apoia.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mds_apoia',
+        'USER': 'apoia_usuario',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
