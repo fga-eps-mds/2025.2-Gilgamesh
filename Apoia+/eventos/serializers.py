2599a16a7df271e78661c7c594ef4d77b6a69bfb
Thauany Vieira <vieirabrito.thauany@gmail.com>
2025-11-15 09:49:11 -0300

    Atualiza serializer de eventos

---
+++ Apoia+/eventos/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Evento
+
+class EventoSerializer(serializers.ModelSerializer):
+    criado_por = serializers.StringRelatedField(read_only=True)
+
+class Meta:
+     model = Evento
+     fields = [
+         'id',
+         'nome',
+         'descricao',
+         'data_inicio',
+         'data_fim',
+         'local',
+         'criado_por',
+         'criado_em',
+         'atualizado_em',
+    ]
+read_only_fields = ['criado_por', 'criado_em', 'atualizado_em']
