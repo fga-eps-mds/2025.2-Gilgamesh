e9ea59f6d8d0c1b7ae98aec833e528944fc2df87
MLuana725 <marialuana.sl962@gmail.com>
2025-11-23 20:30:51 -0300

    Código do widget Event_Card: "cartas" para vizualizar eventos disponiveis na página de lista de eventos

--- mobile_apoia/lib/widgets/event_card.dart
+++ mobile_apoia/lib/widgets/event_card.dart
@@ -9,6 +9,55 @@ class EventCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card();
+    return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      elevation: 4,
+      margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      child: InkWell(
+        onTap: onTap,
+        borderRadius: BorderRadius.circular(16),
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(event.nome, style: Theme.of(context).textTheme.titleLarge),
+              const SizedBox(height: 6),
+
+              Text(
+                event.descricao,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+              ),
+              const SizedBox(height: 8),
+
+              Row(
+                children: [
+                  const Icon(Icons.calendar_month, size: 20),
+                  const SizedBox(width: 6),
+                  Text(
+                    "${event.date.day}/${event.date.month}/${event.date.year}",
+                  ),
+                ],
+              ),
+
+              Row(
+                children: [
+                  Icon(Icons.location_on_outlined, size: 18),
+                  const SizedBox(width: 4),
+                  Expanded(
+                    child: Text(
+                      event.location,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
