8b5d69e28a4a186fbfc46279667c259b9e983dcb
PauloViniciusP <nexz265@gmail.com>
2025-11-28 21:42:03 -0300

    test: atualiza factories para compatibilidade com novo model de Usuario e campos obrigatorios

--- Apoia+/tests/autenticacao/factories.py
+++ Apoia+/tests/autenticacao/factories.py
@@ -5,9 +5,16 @@ from autenticacao.models import Usuario
 class UsuarioFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Usuario
+        # Garante que se chamar a factory 2x com mesmo email, ele pega o existente
+        django_get_or_create = ('email',)
 
     nome = factory.Faker('name', locale='pt_BR')
     email = factory.Sequence(lambda n: f'usuario{n}@exemplo.com')
-    # Gera o hash da senha automaticamente
-    senha = factory.LazyFunction(lambda: make_password('senha_padrao_123'))
+    
+    # O campo agora é 'password' (padrão do AbstractBaseUser) e não 'senha'
+    password = factory.LazyFunction(lambda: make_password('senha_padrao_123'))
+    
     tipo_usuario = 'voluntario'
+    
+    # O model exige UF e não pode ser nulo
+    uf = 'DF'
--- Apoia+/tests/participacoes/factories.py
+++ Apoia+/tests/participacoes/factories.py
@@ -1,6 +1,5 @@
 import factory
 from participacoes.models import Participacao
-# Importando factories de outros módulos para criar as relações
 from tests.autenticacao.factories import UsuarioFactory
 from tests.eventos.factories import EventoFactory
 
@@ -8,7 +7,8 @@ class ParticipacaoFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Participacao
 
-    # Cria as chaves estrangeiras automaticamente
+    # Usa a factory corrigida acima
     usuario = factory.SubFactory(UsuarioFactory)
     evento = factory.SubFactory(EventoFactory)
+    
     status_confirmacao = 'pendente'
