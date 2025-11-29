6152d17823d43b47b1f4b80eccbb2174a098665a
PauloViniciusP <nexz265@gmail.com>
2025-11-28 22:12:50 -0300

    test(autenticacao): adiciona testes de cadastro (ONG/Voluntario) e rota correspondente

--- Apoia+/autenticacao/urls.py
+++ Apoia+/autenticacao/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from .views import LoginView, LogoutView, CadastroView
 
-
 urlpatterns = [
-    path('registro/', CadastroView.as_view(), name='registro'),
+    # MUDANÇA AQUI: de 'registro' para 'cadastro'
+    path('cadastro/', CadastroView.as_view(), name='cadastro'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
