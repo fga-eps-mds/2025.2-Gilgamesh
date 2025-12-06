11da9ec4863d1ff5c20b8b560cc2dd2b949efee5
edso-n <232025730@aluno.unb.br>
2025-12-06 09:16:42 -0300

    fix: Correção de rotas da nav bar e home para tela de ong

--- mobile_apoia/lib/widgets/barra_inferior_e_superior.dart
+++ mobile_apoia/lib/widgets/barra_inferior_e_superior.dart
@@ -1,11 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_apoia/telas/ongs/tela_perfil_ong.dart';
 import 'package:mobile_apoia/widgets/logo.dart';
 import '../telas/home_ongs.dart';
 
 //Barra inferior de navegação
+// Cores (Tema Doador)
+const Color corAzulPrincipal = Color(0xFF007AFF);
+const Color corLaranjaONG = Color(0xFFFF9900);
+
+//  BARRA SUPERIOR
+class SuperiorBar extends StatelessWidget implements PreferredSizeWidget {
+  final bool showBackButton;
+
+  const SuperiorBar({super.key, this.showBackButton = false});
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: corLaranjaONG,
+      elevation: 0,
+
+      leading: showBackButton
+          ? IconButton(
+              icon: const Icon(Icons.arrow_back, color: Colors.white),
+              onPressed: () => Navigator.of(context).pop(),
+            )
+          : null,
+
+      title: const Logo(),
+      centerTitle: true,
+
+      automaticallyImplyLeading: false,
+    );
+  }
+}
+
 class BottomNavBar extends StatelessWidget {
   final int iconSelecionado;
-  final Function(int) onTap;
+  final ValueChanged<int> onTap;
 
   const BottomNavBar({
     super.key,
@@ -15,53 +50,48 @@ class BottomNavBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 40),
-      height: 75,
-      decoration: const BoxDecoration(color: Color(0xFFE2952A)),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          IconButton(
-            icon: Icon(
-              Icons.home,
-              color: iconSelecionado == 0 ? Colors.white : Colors.white70,
-              size: 28,
-            ),
-            onPressed: () => Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(builder: (_) => const HomeOngs()),
-            ),
-          ),
+    return BottomNavigationBar(
+      backgroundColor: corLaranjaONG,
 
-          IconButton(
-            icon: Icon(
-              Icons.person,
-              color: iconSelecionado == 2 ? Colors.white : Colors.white70,
-              size: 28,
-            ),
-            onPressed: () => onTap(2), //tela do perfil ong aqui
+      currentIndex: iconSelecionado,
+
+      onTap: (index) {
+        onTap(index);
+
+        if (index == 0) {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (_) => const HomeOngs()),
+          );
+        } else if (index == 2) {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (_) => const TelaPerfilONG()),
+          );
+        }
+      },
+      items: <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.home,
+            color: iconSelecionado == 0 ? Colors.white : Colors.white70,
           ),
-        ],
-      ),
+          label: 'Inicio',
+        ),
+
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.person,
+            color: iconSelecionado == 1 ? Colors.white : Colors.white70,
+          ),
+          label: 'Perfil',
+        ),
+      ],
+
+      selectedItemColor: Colors.white,
+      unselectedItemColor: Colors.white70,
+      showUnselectedLabels: true,
+      type: BottomNavigationBarType.fixed,
     );
   }
-}
-
-//Barra superior
-class SuperiorBar extends StatelessWidget implements PreferredSizeWidget {
-  const SuperiorBar({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return AppBar(
-      backgroundColor: const Color(0xFFE2952A),
-      elevation: 0,
-      centerTitle: true,
-      title: Logo(),
-    );
-  }
-
-  @override
-  Size get preferredSize => const Size.fromHeight(60);
 }
