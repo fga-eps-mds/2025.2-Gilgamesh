d65b687442f787cb7d5d5781a6be378bdd833da2
edso-n <232025730@aluno.unb.br>
2025-12-10 15:07:04 -0300

    fix: Correção de urls para receber lista de eventos e ongs cadastradas

--- Apoia+/autenticacao/urls.py
+++ Apoia+/autenticacao/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import LoginView, LogoutView, CadastroView
+from .views import LoginView, LogoutView, CadastroView, ListaOngsView
 
 urlpatterns = [
     # MUDANÇA AQUI: de 'registro' para 'cadastro'
     path('cadastro/', CadastroView.as_view(), name='cadastro'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('ongs/', ListaOngsView.as_view(), name='lista-ongs'),
 ]
