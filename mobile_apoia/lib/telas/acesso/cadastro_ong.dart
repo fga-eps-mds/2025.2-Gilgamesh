25d8873f2f966c4f875b9e0068f53c93b04c2c96
Guilherme <gui16carvalho@gmail.com>
2025-12-12 23:33:36 -0300

    feat(ui): adiciona botão de retorno no cadastro

--- mobile_apoia/lib/telas/acesso/cadastro_ong.dart
+++ mobile_apoia/lib/telas/acesso/cadastro_ong.dart
@@ -188,6 +188,17 @@ class _CadastroOngsState extends State<CadastroOngs> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.orange),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+      ),
+
+
       body: SingleChildScrollView(
         child: SafeArea(
           child: Padding(
